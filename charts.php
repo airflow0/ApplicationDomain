b261a322218ec76039b707b7b38b5460f9ebe775
tanner-j <tjohn412@students.kennesaw.edu>
2020-03-11 17:15:18 -0400

    reports link and modal added

--- charts.php
+++ charts.php
@@ -314,7 +314,7 @@ $revenue->setFetchMode(PDO::FETCH_ASSOC);
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="addAccountModalLabel">Add New Account</h5>
+                <h5 class="modal-title" id="addAccountModalLabel">Edit Account</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
