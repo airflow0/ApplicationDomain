2d5e7fcb0137e2587e3a2f6515684f2cdf70a730
airflow0 <airflow0@gmail.com>
2020-04-08 22:23:05 -0400

    push

--- edit_journal.php
+++ edit_journal.php
@@ -149,13 +149,21 @@ function updateBalance(PDO $pdo, $referenceID)
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.20/af-2.3.4/b-1.6.1/b-colvis-1.6.1/b-flash-1.6.1/b-html5-1.6.1/b-print-1.6.1/cr-1.5.2/fc-3.3.0/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/rr-1.2.6/sc-2.0.1/sp-1.0.1/sl-1.3.1/datatables.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.20/af-2.3.4/b-1.6.1/b-colvis-1.6.1/b-flash-1.6.1/b-html5-1.6.1/b-print-1.6.1/cr-1.5.2/fc-3.3.0/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/rr-1.2.6/sc-2.0.1/sp-1.0.1/sl-1.3.1/datatables.min.css"/>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
 
     <title>CountOnUs - Journal Entry</title>
     <script type="text/javascript">
         $(document).ready(function () {
-            var table = $('#journal-table-view').DataTable();
+            var table = $('#journal-table-view').DataTable({
+                dom: 'lfBrtip',
+                buttons: [
+                    'print'
+                ]
+            });
+            $("#journal-table-view_wrapper > .dt-buttons").appendTo("div.p-2");
             var referenceID = '<?php echo $referenceID ?>';
             $('#journal-table-view tbody').on('click', 'tr', function () {
                 if ($(this).hasClass('selected')) {
@@ -316,6 +324,7 @@ function updateBalance(PDO $pdo, $referenceID)
             <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalAddLine">Add line</button>
             <button type="button" class="btn btn-secondary" data-toggle="modal" id="editLine">Edit line</button>
             <button type="button" class="btn btn-secondary" data-toggle="modal" id="deleteLine">Delete line</button>
+
         </div>
     </div>
 
